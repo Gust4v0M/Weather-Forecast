e9cadf349eab96669d6e26fc1bd6586dcc2b0527
Gust4v0M <gustavomorgado6@gmail.com>
2024-07-11 16:23:22 -0300

    Styulesheet of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherAppComponent } from './weather-app/weather-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherAppComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/weather-app/weather-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-app',
+  templateUrl: './weather-app.component.html',
+  styleUrl: './weather-app.component.css'
+})
+export class WeatherAppComponent {
+
+}
