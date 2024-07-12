c37cb65bc199b30a966ef8bd10145cfa4f1b818d
Gust4v0M <gustavomorgado6@gmail.com>
2024-07-12 00:26:27 -0300

    Part thing of style was done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherAppComponent } from './weather-app/weather-app.component';
+import { ForecastTimeComponent } from './forecast-time/forecast-time.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherAppComponent
+    WeatherAppComponent,
+    ForecastTimeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/forecast-time/forecast-time.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForecastTimeComponent } from './forecast-time.component';
+
+describe('ForecastTimeComponent', () => {
+  let component: ForecastTimeComponent;
+  let fixture: ComponentFixture<ForecastTimeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ForecastTimeComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ForecastTimeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
