d8b81f0bc6d2b9fb4895228d4287eee2f03486a5
Gust4v0M <gustavomorgado6@gmail.com>
2024-07-20 23:08:28 -0300

    Table done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { SearchWeatherComponent } from './search-weather/search-weather.component';
+import { WeatherAppComponent } from './weather-app/weather-app.component';
 
 const routes: Routes = [
   {path: '', component: WelcomePageComponent},
-  { path: 'searchWeather', component: SearchWeatherComponent }
+  { path: 'searchWeather', component: SearchWeatherComponent },
+  { path: 'weather-app', component: WeatherAppComponent}
 
 ];
 
